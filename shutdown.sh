d340068a7df0289e1393c3dcaba0607ff9b659e3
Gary Archer <32388530+gary-archer@users.noreply.github.com>
2021-11-05 21:22:27 +0000

    Delete shutdown.sh

--- shutdown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-##################################################
-# Free Docker resources when finished with the API
-##################################################
-
-docker compose -f delivery/docker-localweb/docker-compose.yml down

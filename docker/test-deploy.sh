89b61ff2b7ffe49bc7fa070368ef2311c096aedc
gary <gary@MACSTATION.local>
2022-05-25 18:47:13 +0100

    Updates

---
+++ docker/test-deploy.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+##############################################################
+# A script to test Docker deployment on a development computer
+##############################################################
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+cd ..
+
+#
+# Install dependencies
+#
+if [ ! -d 'node_modules' ]; then
+  
+  #rm -rf node_modules
+  #npm install
+  if [ $? -ne 0 ]; then
+    echo "Problem encountered installing the OAuth Agent dependencies"
+    exit
+  fi
+fi
+
+#
+# Build to the dist folder
+#
+#npm run buildRelease
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered building the OAuth Agent code'
+  exit
+fi
+
+#
+# Build the docker image
+#
+docker build -f docker/Dockerfile --build-arg TRUSTED_CA_CERTS='docker/trusted.ca.pem' -t oauthagent:v1 .
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered building the OAuth Agent docker image'
+  exit
+fi
+
+#
+# Prepare root CA certificates that the Docker container will trust
+#
+cp ./certs/authsamples-dev.ca.pem docker/trusted.ca.pem
+
+#
+# Run the docker deployment
+#
+docker compose --file docker/docker-compose.yml --project-name oauthagent up --force-recreate --detach
+if [ $? -ne 0 ]; then
+  echo "Problem encountered running Docker image"
+  exit 1
+fi
+
+#
+# Wait for it to become available
+#
+echo 'Waiting for OAuth Agent to become available ...'
+WEB_ORIGIN='https://web.authsamples-dev.com'
+BASE_URL='https://localtokenhandler.authsamples-dev.com:444'
+while [ "$(curl -k -s -X POST -H "origin:$WEB_ORIGIN" -o /dev/null -w ''%{http_code}'' "$BASE_URL/oauth-agent/login/start")" != '200' ]; do
+  sleep 2
+done
--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ if [ ! -d 'certs' ]; then
   git clone https://github.com/gary-archer/oauth.developmentcertificates ./resources
   if [ $? -ne 0 ]; then
     echo 'Problem encountered downloading OAuth Agent certificates'
-    exit 1
+    exit
   fi
 
   #
@@ -39,7 +39,7 @@ if [ ! -d 'node_modules' ]; then
   npm install
   if [ $? -ne 0 ]; then
     echo "Problem encountered installing the OAuth Agent dependencies"
-    exit 1
+    exit
   fi
 fi
 
@@ -51,6 +51,6 @@ fi
 npm start
 if [ $? -ne 0 ]; then
   echo "Problem encountered starting the OAuth Agent"
-  exit 1
+  exit
 fi
 
